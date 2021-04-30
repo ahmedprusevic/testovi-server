cad33ad48a95f1a9559157166bb6a6c9c22dedc9
ahmedprusevic <ahmedprusevic@gmail.com>
2021-04-30 03:43:44 +0200

    working on user model

---
+++ models/User.go
@@ -0,0 +1,104 @@
+package models
+
+import (
+	"errors"
+	"html"
+	"strings"
+	"time"
+
+	"github.com/badoux/checkmail"
+	"github.com/jinzhu/gorm"
+	"golang.org/x/crypto/bcrypt"
+)
+
+type User struct {
+	ID        uint64    `gorm:"primary_key;auto_increment" json:"id"`
+	Name      string    `gorm:"size:255;not null;" json:"name"`
+	Email     string    `gorm:"size:100;not null;unique" json:"email"`
+	Password  string    `gorm:"size:100;not null;" json:"password"`
+	Role      string    `gorm:"size:100;not null;" json:"role"`
+	CreatedAt time.Time `gorm:"default:CURRENT_TIMESTAMP" json:"created_at"`
+}
+
+func VerifyPassword(hashedPassword, password string) error {
+	return bcrypt.CompareHashAndPassword([]byte(hashedPassword), []byte(password))
+}
+
+func (u *User) HashPassword() error {
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(u.Password), bcrypt.DefaultCost)
+	if err != nil {
+		return err
+	}
+	u.Password = string(hashedPassword)
+	return nil
+}
+
+func (u *User) FillFields() {
+	u.ID = 0
+	u.Name = html.EscapeString(strings.TrimSpace(u.Name))
+	u.Email = html.EscapeString(strings.TrimSpace(u.Email))
+	u.CreatedAt = time.Now()
+}
+
+func (u *User) Validate(action string) error {
+	switch strings.ToLower(action) {
+	case "update":
+		if u.Name == "" {
+			return errors.New("name is required")
+		}
+		if u.Password == "" {
+			return errors.New("password is required")
+		}
+		if u.Email == "" {
+			return errors.New("email is required")
+		}
+		if err := checkmail.ValidateFormat(u.Email); err != nil {
+			return errors.New("please input valid email")
+		}
+		return nil
+	case "login":
+		if u.Password == "" {
+			return errors.New("password is required")
+		}
+		if u.Email == "" {
+			return errors.New("email is required")
+		}
+		if err := checkmail.ValidateFormat(u.Email); err != nil {
+			return errors.New("please input valid email")
+		}
+		return nil
+	default:
+		if u.Name == "" {
+			return errors.New("name is required")
+		}
+		if u.Password == "" {
+			return errors.New("password is required")
+		}
+		if u.Email == "" {
+			return errors.New("email is required")
+		}
+		if err := checkmail.ValidateFormat(u.Email); err != nil {
+			return errors.New("please input valid email")
+		}
+		return nil
+	}
+
+}
+
+func (u *User) SaveUser(db *gorm.DB) (*User, error) {
+	err := db.Debug().Create(&u).Error
+	if err != nil {
+		return &User{}, err
+	}
+	return u, nil
+}
+
+func (u *User) FindAllUsers(db *gorm.DB) (*[]User, error) {
+	users := []User{}
+	err := db.Debug().Model(&User{}).Limit(100).Find(&users).Error
+	if err != nil {
+		return &[]User{}, err
+	}
+	return &users, err
+
+}
